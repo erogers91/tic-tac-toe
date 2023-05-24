a0488b493f1083e93766fb6ca3dfec1e15f73fb1
Eric Rogers <47755098+erogers91@users.noreply.github.com>
2023-05-24 10:15:32 -0500

    Adding Firebase support

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import TicTacToe from './TicTacToe';
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCXSa8YS7yAkwlqJapg5ryHOYVSOT-VPLE",
+  authDomain: "tic-tac-toes1.firebaseapp.com",
+  projectId: "tic-tac-toes1",
+  storageBucket: "tic-tac-toes1.appspot.com",
+  messagingSenderId: "963118357749",
+  appId: "1:963118357749:web:a561be00dfbc35e8ab79bc",
+  measurementId: "G-PKCD1JS39L"
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -8,3 +19,6 @@ root.render(
     <TicTacToe />
   </React.StrictMode>
 );
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
